8572dfb82b9d5a30c8cdf23433d9e405f484823d
Abhik Khanra <abhik@abhikrk.in>
2024-02-27 23:01:03 +0530

    Initial setup for managing hosts with Nix
    Uses Homebrew for managing GUI apps

---
+++ flake.nix
@@ -0,0 +1,151 @@
+# ~/setup/flake.nix
+
+{
+  description = "My system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+        url = "github:LnL7/nix-darwin";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  let
+    configuration = {pkgs, ... }: {
+
+	services.nix-daemon.enable = true;
+	# Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility. please read the changelog
+        # before changing: `darwin-rebuild changelog`.
+        system.stateVersion = 4;
+
+        # The platform the configuration will be used on.
+        # If you're on an Intel system, replace with "x86_64-darwin"
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+	# Allow unfree packages
+	nixpkgs.config.allowUnfree = true;
+
+        # Declare the user that will be running `nix-darwin`.
+        users.users.trycatcher = {
+            name = "trycatcher";
+            home = "/Users/trycatcher";
+        };
+
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true;
+
+        environment.systemPackages = with pkgs; [];
+
+	homebrew = {
+	    enable = true;
+            onActivation.cleanup = "uninstall";
+	    taps = [
+		"homebrew/cask-fonts"
+		"railwaycat/emacsmacport"
+	    ];
+	    casks = [
+		"emacs-mac"
+		"dropbox"
+		"wezterm"
+		"font-fira-code"
+		"font-iosevka"
+		"microsoft-excel"
+		"anki"
+		"obsidian"
+		"bitwarden"
+		"calibre"
+		"spotify"
+		"netnewswire"
+		"zotero"
+		"slack"
+		"zulip"
+		"discord"
+		"element"
+		"firefox"
+		"google-chrome"
+	    ];
+
+	};
+
+	security.pam.enableSudoTouchIdAuth = true;
+    };
+    homeconfig = {pkgs, config, ...}: {
+	# this is internal compatibility configuration 
+	# for home-manager, don't change this!
+	home.stateVersion = "23.05";
+	# Let home-manager install and manage itself.
+	programs.home-manager.enable = true;
+
+        programs.neovim = {
+	    enable = true;
+            viAlias = true;
+	    vimAlias = true;
+	};
+
+	programs.zsh = {
+  	    enable = true;
+    	    shellAliases = {
+        	rebuild-setup = "darwin-rebuild switch --flake ~/setup";
+    	    };
+	};
+
+	programs.git = {
+  	    enable = true;
+    	    userName = "Abhik Khanra";
+    	    userEmail = "abhik@abhikrk.in";
+    	    ignores = [ ".DS_Store" ];
+    	    extraConfig = {
+        	init.defaultBranch = "main";
+        	push.autoSetupRemote = true;
+    	    };
+	};
+
+	home.file = {
+    	    "./.config/nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink ../dotfiles/neovim/config.lua;
+	    ".wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink ..dotfiles/wezterm/config.lua;
+	    "./.config/tmuxp/session.yaml".source = config.lib.file.mkOutOfStoreSymlink ..dotfiles/tmuxp/config.yaml;
+	    "init.el".source = config.lib.file.mkOutOfStoreSymlink ...emacs.d/init.el;
+  	};
+
+	home.packages = with pkgs; [
+	    neofetch
+	    wget
+	    ledger
+	    ripgrep
+	    tmux
+	    tmuxp
+	    tarsnap
+	    colima
+	    docker
+	    docker-compose
+	    gh
+	];
+	
+	home.sessionVariables.EDITOR = "nvim";
+    };
+  in
+  {
+    darwinConfigurations."maclatakan" = nix-darwin.lib.darwinSystem {
+      modules = [
+         configuration
+	 home-manager.darwinModules.home-manager  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+            home-manager.verbose = true;
+            home-manager.users.trycatcher = homeconfig;
+        }
+      ];
+    };
+  };
+}
